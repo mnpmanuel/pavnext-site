45542a68fc1bd0a7df8186023c5fc563b4a344a3
bu11it <mnp.manuel@gmail.com>
2017-08-02 11:41:51 +0100

    Updated website
    Optimized CSS to better work with more images using boostrap columns.
    
    Added more partners.
    
    Added location to footer.
    
    TODO:
    - Adjust image sizes and compress them
    - Adjust margin in "Partners"

--- js/pavnext.js
+++ js/pavnext.js
@@ -8,3 +8,52 @@ $(function() {
         event.preventDefault();
     });
 });
+
+$(document).ready(function(){
+	// Initialize WOW.js Scrolling Animations
+    new WOW().init();
+});
+
+$(document).ready(function(){
+    $('.awesome-tooltip').tooltip({
+        placement: 'left'
+    });
+});
+
+$(document).ready(function(){
+	$('#dot-nav li').click(function(){
+      
+        var id = $(this).find('a').attr("href"),
+          posi,
+          ele,
+          padding = 0;
+      
+        ele = $(id);
+        posi = ($(ele).offset()||0).top - padding;
+      
+        $('html, body').animate({scrollTop:posi}, 'slow');
+      
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+
+});
